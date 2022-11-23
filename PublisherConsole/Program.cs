0f3565803c6c2e350b5085419e9a701f5e777017
Colt Skeen <colt.skeen@gesa.com>
2022-11-23 11:17:36 -0800

    Created Db with some data from console program

--- PublisherConsole/Program.cs
+++ PublisherConsole/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PublisherData;
+using PublisherDomain;
+
+using (PubContext context = new PubContext())
+{
+    context.Database.EnsureCreated();
+}
+
+GetAuthors();
+AddAuthors();
+GetAuthors();
+
+void AddAuthors()
+{
+    var author = new Author { FirstName = "Freddie", LastName = "Skeen" };
+    using var context = new PubContext();
+    context.Authors.Add(author);
+    context.SaveChanges();
+}
+
+void GetAuthors()
+{
+    using var context = new PubContext();
+    var authors = context.Authors.ToList();
+    foreach (var author in authors)
+    {
+        Console.WriteLine(author.FirstName + " " + author.LastName);
+    }
+}
---
+++ PublisherData/PubContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PublisherDomain;
+
+namespace PublisherData;
+
+public class PubContext:DbContext
+{
+    public DbSet<Author> Authors { get; set; }
+    public DbSet<Book> Books { get; set; }
+
+    /* Hardcoding this connection string here for demo -- DONT DO THIS FOR PROD APP*/
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = PubDatabase");
+    }
+}
